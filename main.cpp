9d572edab7d6733a56e3df379403793fa8d1263c
subitooo <ishkaeva.vika@mail.ru>
2024-05-13 20:54:54 +0200

    Добавлен пример CMakeLists.txt

---
+++ main.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+
+class Rectangle {
+    float m_a;
+    float m_b;
+    float m_perimetr;
+
+
+public:
+    Rectangle(float a, float b) 
+    {
+        m_a = a;
+        m_b = b;
+    }
+
+    void setA(float a) 
+    {
+        m_a = a;
+
+    }
+
+    void setB(float b) 
+    {
+        m_b = b;
+    }
+
+    float Ploshad() 
+    {
+        return m_a * m_b;
+    }
+
+    float Perimetr() 
+    {
+        return 2 * (m_a + m_b);
+    }
+
+    float Diagonal() 
+    {
+        return sqrt(m_a * m_a + m_b * m_b);
+    }
+
+
+};
+
+
+int main() {
+
+    setlocale(LC_ALL, "Russian");
+
+    Rectangle r1(15.f, 7.f);
+
+    std::cout << r1.Ploshad() << std::endl;
+    std::cout << r1.Perimetr() << std::endl;
+    std::cout << r1.Diagonal() << std::endl;
+
+    return 0;
+}
